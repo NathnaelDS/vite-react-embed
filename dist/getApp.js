8d89067adf7410e7b20bfeb48edf2b76c24f6117
NathnaelDS <nathnaelds@gmail.com>
2021-08-08 20:36:03 +0300

    dynamically add app entry point

--- dist/getApp.js
+++ dist/getApp.js
@@ -1,15 +1,22 @@
-var appScript = document.createElement("script"); // create a script DOM node
-appScript.type = "module";
-appScript.crossOrigin = ""; // same as anonymous
-appScript.src = "/assets/index.2b0c28b9.js"; // set its src to the provided URL
-document.head.appendChild(appScript);
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+  var entryPoint = document.createElement("div");
+  entryPoint.id = "root";
+  document.getElementsByClassName("section")[0].after(entryPoint);
 
-var libScript = document.createElement("link"); // create a script DOM node
-libScript.rel = "modulepreload";
-libScript.href = "/assets/vendor.86d874f6.js"; // set its src to the provided URL
-document.head.appendChild(libScript);
+  var appScript = document.createElement("script"); // create a script DOM node
+  appScript.type = "module";
+  appScript.crossOrigin = ""; // same as anonymous
+  appScript.src = "/assets/index.2b0c28b9.js"; // set its src to the provided URL
+  document.head.appendChild(appScript);
 
-var css = document.createElement("link"); // create a script DOM node
-css.rel = "stylesheet";
-css.href = "/assets/index.0673ce28.css"; // set its src to the provided URL
-document.head.appendChild(css);
+  var libScript = document.createElement("link"); // create a script DOM node
+  libScript.rel = "modulepreload";
+  libScript.href = "/assets/vendor.86d874f6.js"; // set its src to the provided URL
+  document.head.appendChild(libScript);
+
+  var css = document.createElement("link"); // create a script DOM node
+  css.rel = "stylesheet";
+  css.href = "/assets/index.0673ce28.css"; // set its src to the provided URL
+  document.head.appendChild(css);
+}
